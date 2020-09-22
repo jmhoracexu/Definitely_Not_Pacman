1e70c7bf27bbd794afe3fa1cf0075a7271bfc8a9
99156186 <99156186@student.uts.edu.au>
2020-09-22 21:11:19 +1000

    Mid way commit

---
+++ Assets/Scripts/AudioControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioControl : MonoBehaviour
+{
+    public AudioClip intro;
+    public AudioClip Ghost_Normal;
+    void Start()
+    {
+        //GetComponent<AudioSource>().loop = true;
+        StartCoroutine(PlayAudios());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator PlayAudios()
+    {
+        GetComponent<AudioSource>().clip = intro;
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(intro.length);
+        GetComponent<AudioSource>().clip = Ghost_Normal;
+        GetComponent<AudioSource>().Play();
+        GetComponent<AudioSource>().loop = true;
+    }
+}
